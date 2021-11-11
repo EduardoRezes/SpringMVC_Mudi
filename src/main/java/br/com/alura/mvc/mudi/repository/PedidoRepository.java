256c5e3a63e202c997a346ced6bdecfb245360e1
Eduardo Rezes <contatoeduardorezes@gmail.com>
2021-11-11 10:23:53 -0300

    feature/0003 - Aplicando cache na aplicação

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,8 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
+	//Necessário fazer a anotação @EnableCaching na classe principal(MudiApplication) para que o cache funcione.
+	@Cacheable("Itens")
 	List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
 	@Query("SELECT p FROM Pedido p join p.user u WHERE u.username = :username ")
