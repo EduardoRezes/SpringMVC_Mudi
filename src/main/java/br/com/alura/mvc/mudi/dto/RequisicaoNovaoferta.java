5027eb27b349e2617297e46d56671075581ac9b2
Eduardo Rezes <contatoeduardorezes@gmail.com>
2021-11-12 15:52:36 -0300

    feature/0006 - Criada nova requisição para Ofertas de usuarios a pedidos

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaoferta.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaoferta.java
@@ -4,15 +4,26 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import br.com.alura.mvc.mudi.model.Oferta;
 
-public class RequisicaoNovaoferta {
+public class RequisicaoNovaOferta {
 	
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
+
 	private Long pedidoId;
+	
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")//começa com digito \d+ => significa (varios digito)
+	@NotNull
 	private String valor;
+	
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
+	@NotNull
 	private String dataDaEntrega;
+	
 	private String comentario;
 	
 	public Long getPedidoId() {
